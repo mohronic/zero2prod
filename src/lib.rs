29f09a1383421d6fac1bc8392c241196baa3880c
Kristian Mohr Nielsen <kmn1993@gmail.com>
2021-09-19 19:01:35 +0200

    Fix formatting

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,15 @@
-use std::net::TcpListener;
-use actix_web::{App, HttpResponse, HttpServer, Responder, web};
 use actix_web::dev::Server;
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use std::net::TcpListener;
 
 async fn health_check() -> impl Responder {
     HttpResponse::Ok().finish()
 }
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
-        App::new()
-            .route("/health_check", web::get().to(health_check))
-    })
-    .listen(listener)?
-    .run();
+    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
+        .listen(listener)?
+        .run();
 
     Ok(server)
 }
--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,7 @@ use zero2prod::run;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:8000")
-    .expect("Failed to bind address");
-    
+    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind address");
+
     run(listener)?.await
 }
