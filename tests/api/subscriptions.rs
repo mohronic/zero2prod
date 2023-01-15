2f39946ac5f8bd4f5f83fd21aa29b5f2070c2e17
Kristian Mohr Nielsen <kmn1993@gmail.com>
2023-01-15 16:52:58 +0100

    set correct status for new subscriber

--- tests/api/subscriptions.rs
+++ tests/api/subscriptions.rs
@@ -20,14 +20,32 @@ async fn subscripe_returns_200_for_valid_form_data() {
     let respone = app.post_subscriptions(body.into()).await;
 
     assert_eq!(200, respone.status().as_u16());
+}
 
-    let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
+#[tokio::test]
+async fn subscripe_persists_the_new_subscriber() {
+    let app = spawn_app().await;
+    let body = "name=john%20doe&email=john_doe%40gmail.com";
+
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .expect(1)
+        .mount(&app.email_server)
+        .await;
+
+    let respone = app.post_subscriptions(body.into()).await;
+
+    assert_eq!(200, respone.status().as_u16());
+
+    let saved = sqlx::query!("SELECT email, name, status FROM subscriptions",)
         .fetch_one(&app.db_pool)
         .await
         .expect("Failed to fetch saved subscription.");
 
     assert_eq!(saved.email, "john_doe@gmail.com");
     assert_eq!(saved.name, "john doe");
+    assert_eq!(saved.status, "pending_confirmation");
 }
 
 #[tokio::test]
