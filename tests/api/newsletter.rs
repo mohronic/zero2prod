5242c4ff7b1512f827a90c3cde6048be97704779
Kristian Mohr Nielsen <kmn1993@gmail.com>
2023-03-20 20:18:51 +0100

    newsletter test failing

---
+++ tests/api/newsletter.rs
@@ -0,0 +1,52 @@
+use wiremock::{
+    matchers::{any, method, path},
+    Mock, ResponseTemplate,
+};
+
+use crate::helpers::{spawn_app, TestApp};
+
+#[tokio::test]
+async fn newsletters_are_not_delivered_to_unconfirmed_subscribers() {
+    let app = spawn_app().await;
+    create_unconfirmed_subscriber(&app).await;
+
+    Mock::given(any())
+        .respond_with(ResponseTemplate::new(200))
+        .expect(0)
+        .mount(&app.email_server)
+        .await;
+
+    let newsletter_request_body = serde_json::json!({
+        "title": "Newsletter title",
+        "content": {
+            "text": "Newsletter body as plain text",
+            "html": "<p>Newsletter body as html</p>"
+        }
+    });
+
+    let response = reqwest::Client::new()
+        .post(&format!("{}/newsletters", &app.address))
+        .json(&newsletter_request_body)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    assert_eq!(response.status().as_u16(), 200);
+}
+
+async fn create_unconfirmed_subscriber(app: &TestApp) {
+    let body = "name=john%20doe&email=john_doe%40gmail.com";
+
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .expect(1)
+        .mount(&app.email_server)
+        .await;
+
+    let respone = app
+        .post_subscriptions(body.into())
+        .await
+        .error_for_status()
+        .unwrap();
+}
