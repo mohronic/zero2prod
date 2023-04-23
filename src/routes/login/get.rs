b3ac7a5824073f0d1ae7b39ad9059ec0b0c3d399
Kristian Mohr Nielsen <kmn1993@gmail.com>
2023-04-23 10:06:51 +0200

    add hmac tag to error query param

--- src/routes/login/get.rs
+++ src/routes/login/get.rs
@@ -1,4 +1,4 @@
-use actix_web::{http::header::ContentType, HttpResponse, web};
+use actix_web::{http::header::ContentType, web, HttpResponse};
 
 #[derive(serde::Deserialize)]
 pub struct QueryParams {
@@ -8,9 +8,11 @@ pub struct QueryParams {
 pub async fn login_form(query: web::Query<QueryParams>) -> HttpResponse {
     let error_html = match query.0.error {
         None => "".into(),
-        Some(error_message) => format!("<p><i>{error_message}</i></p>"),
+        Some(error_message) => format!(
+            "<p><i>{}</i></p>",
+            htmlescape::encode_minimal(&error_message)
+        ),
     };
-
 
     HttpResponse::Ok()
         .content_type(ContentType::html())
