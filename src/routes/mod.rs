cf937248e6a477085b462cea29da8c7177f59c0d
Kristian Mohr Nielsen <kmn1993@gmail.com>
2023-04-23 08:40:25 +0200

    setup login page

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -3,9 +3,11 @@ mod newsletters;
 mod subscriptions;
 mod subscriptions_confirm;
 mod home;
+mod login;
 
 pub use health_check::*;
 pub use newsletters::*;
 pub use subscriptions::*;
 pub use subscriptions_confirm::*;
 pub use home::*;
+pub use login::*;
--- src/startup.rs
+++ src/startup.rs
@@ -1,6 +1,6 @@
 use crate::configuration::{DatabaseSettings, Settings};
 use crate::email_client::EmailClient;
-use crate::routes::{confirm, health_check, publish_newsletter, subscribe, home};
+use crate::routes::{confirm, health_check, publish_newsletter, subscribe, home, login_form, login};
 use actix_web::dev::Server;
 use actix_web::web::Data;
 use actix_web::{web, App, HttpServer};
@@ -81,6 +81,8 @@ pub fn run(
             .route("/subscriptions/confirm", web::get().to(confirm))
             .route("/newsletters", web::post().to(publish_newsletter))
             .route("/", web::get().to(home))
+            .route("/login", web::get().to(login_form))
+            .route("/login", web::post().to(login))
             .app_data(db_pool.clone())
             .app_data(email_client.clone())
             .app_data(base_url.clone())
