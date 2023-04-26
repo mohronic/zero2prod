19a971118c33ac6c4e31bd4236f85e25f180ec08
Kristian Mohr Nielsen <kmn1993@gmail.com>
2023-04-26 20:51:24 +0200

    use add removal cookie

--- src/routes/login/get.rs
+++ src/routes/login/get.rs
@@ -12,9 +12,8 @@ pub async fn login_form(request: HttpRequest) -> HttpResponse {
         }
     };
 
-    HttpResponse::Ok()
+    let mut response = HttpResponse::Ok()
         .content_type(ContentType::html())
-        .cookie(Cookie::build("_flash", "").max_age(Duration::ZERO).finish())
         .body(format!(
             r#"
             <!DOCTYPE html>
@@ -39,5 +38,9 @@ pub async fn login_form(request: HttpRequest) -> HttpResponse {
                 </body>
             </html>
             "#
-        ))
+        ));
+
+    response.add_removal_cookie(&Cookie::new("_flash", "")).unwrap();
+
+    response
 }
