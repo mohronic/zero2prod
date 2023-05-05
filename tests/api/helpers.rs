4ea4aeb52c753832656b5a24eaaf67b2fc53d03d
Kristian Mohr Nielsen <kmn1993@gmail.com>
2023-05-05 21:40:38 +0200

    reset password

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -93,7 +93,7 @@ impl TestApp {
             .expect("Failed to execute request")
     }
 
-    pub async fn get_login_fn(&self) -> String {
+    pub async fn get_login_html(&self) -> String {
         self.api_client
             .get(format!("{}/login", &self.address))
             .send()
@@ -135,6 +135,14 @@ impl TestApp {
         self.api_client
             .post(&format!("{}/admin/password", &self.address))
             .form(body)
+            .send()
+            .await
+            .expect("Failed to execute request")
+    }
+
+    pub async fn post_logout(&self) -> reqwest::Response {
+        self.api_client
+            .post(&format!("{}/admin/logout", &self.address))
             .send()
             .await
             .expect("Failed to execute request")
