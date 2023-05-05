5ffa08e58286ad8310e3e2dd082aed9e025ffd9c
Kristian Mohr Nielsen <kmn1993@gmail.com>
2023-05-05 20:27:46 +0200

    initital change password

---
+++ src/routes/admin/password/get.rs
@@ -0,0 +1,36 @@
+use actix_web::{HttpResponse, http::header::ContentType};
+
+pub async fn change_password_form() -> Result<HttpResponse, actix_web::Error> {
+    Ok(HttpResponse::Ok().content_type(ContentType::html()).body(
+        r#"
+        <!DOCTYPE html>
+        <html lang="en">
+            <head>
+                <meta http-equiv="content-type" content="text/html; charset=utf-8">
+                <title>Change password</title>
+            </head>
+            <body>
+                <form action="/admin/password" method="post">
+                    <label>
+                        Current password
+                        <input type="password" placeholder="Enter current password" name="current_password">
+                    </label>
+                    <br>
+                    <label>
+                        New password
+                        <input type="password" placeholder="Enter new password" name="new_password">
+                    </label>
+                    <br>
+                    <label>
+                        New password check
+                        <input type="password" placeholder="Enter new password again" name="new_password_check">
+                    </label>
+                    <br>
+                    <button type="submit">Change password</button>
+                </form>
+                <p><a href="/admin/password">&lt;- Back</a></p>
+            </body>
+        </html> 
+        "#
+    ))
+}
--- src/startup.rs
+++ src/startup.rs
@@ -1,7 +1,7 @@
 use crate::configuration::{DatabaseSettings, Settings};
 use crate::email_client::EmailClient;
 use crate::routes::{
-    admin_dashboard, confirm, health_check, home, login, login_form, publish_newsletter, subscribe,
+    admin_dashboard, confirm, health_check, home, login, login_form, publish_newsletter, subscribe, change_password_form, change_password,
 };
 use actix_session::storage::RedisSessionStore;
 use actix_session::SessionMiddleware;
@@ -106,6 +106,8 @@ pub async fn run(
             .route("/login", web::get().to(login_form))
             .route("/login", web::post().to(login))
             .route("/admin/dashboard", web::get().to(admin_dashboard))
+            .route("/admin/password", web::get().to(change_password_form))
+            .route("/admin/password", web::post().to(change_password))
             .app_data(db_pool.clone())
             .app_data(email_client.clone())
             .app_data(base_url.clone())
